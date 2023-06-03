0ea9d7e0840de08fff28a445e37c790ce2cd916f
lastZu <cdrom2015@ya.ru>
2023-06-03 21:11:40 +0300

    test

--- src/main/java/org/lastzu/springmvc/HelloController.java
+++ src/main/java/org/lastzu/springmvc/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello1")
+    @GetMapping("/hello")
     public String sayHello() {
         return "hello_world";
     }
