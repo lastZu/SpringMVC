544509667cf6147cef196ca23a981d9811fa398f
lastZu <cdrom2015@ya.ru>
2023-06-08 00:52:54 +0300

    add thymeleaf param

--- src/main/java/org/lastzu/springmvc/controllers/HelloController.java
+++ src/main/java/org/lastzu/springmvc/controllers/HelloController.java
@@ -1,15 +1,25 @@
 package org.lastzu.springmvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
-    @GetMapping("/world")
-    public String sayHello() {
-        return "hello/hello_world";
+    @GetMapping("/person")
+    public String sayHello(@RequestParam(value = "name", required = false) String name,
+                           Model model) {
+
+        if (name == null) {
+            name = "World";
+        }
+
+        model.addAttribute("helloName", "Hello, " + name);
+
+        return "hello/hello";
     }
 
     @GetMapping("/Bella")
