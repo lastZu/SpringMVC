408f897115295cdbbb2c172c8a51c5c59672f5d5
lastZu <cdrom2015@ya.ru>
2023-06-12 14:31:02 +0300

    CRUD + create person

--- src/main/java/org/lastzu/springmvc/controllers/PeopleController.java
+++ src/main/java/org/lastzu/springmvc/controllers/PeopleController.java
@@ -1,16 +1,16 @@
 package org.lastzu.springmvc.controllers;
 
 import org.lastzu.springmvc.dao.PersonDAO;
+import org.lastzu.springmvc.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
+    private static final String CATALOG = "people";
     private final PersonDAO personDAO;
 
     public PeopleController(PersonDAO personDAO) {
@@ -21,7 +21,7 @@ public class PeopleController {
     public String index(Model model) {
         model.addAttribute("people", personDAO.index());
 
-        return "people/index";
+        return CATALOG + "/index";
     }
 
     @GetMapping("/{id}")
@@ -29,7 +29,18 @@ public class PeopleController {
                        Model model) {
         model.addAttribute("person", personDAO.show(id));
 
-        return "people/show";
+        return CATALOG + "/show";
     }
 
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("person", new Person());
+        return CATALOG + "/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
 }
